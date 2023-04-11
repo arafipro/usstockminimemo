8c4bee44db222be5c120c8ba6ccc6d29b518a1e0
Hiroki Chiyoda <contact@arafipro.com>
2023-04-11 20:34:44 +0900

    grid_page.dartのtodoを解決
    残りのtodo
    todo main ListPageとGridPageを設定によって切り替える
    todo edit_page ListPageとGridPageを設定によって切り替える
    todo settings_page ListPageとGridPageを切り替えるスイッチを追加
    todo settings_model ListPageとGridPageを切り替える初期値を追加

--- lib/views/grid_page.dart
+++ lib/views/grid_page.dart
@@ -32,46 +32,106 @@ class _GridPageState extends State<GridPage> {
   @override
   Widget build(BuildContext context) {
     const bool isButtonMode = false;
-    // todo1 背景色を修正
     return Scaffold(
+      backgroundColor: bgColor,
       appBar: AppBar(
         automaticallyImplyLeading: false, // 戻るボタンを表示しない
-        // todo2 配色とテキストを修正
-        title: const Text("GridPage"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(1.0),
-        child: MasonryGridView.count(
-          crossAxisCount: 2,
-          itemCount: stockmemos.length,
-          itemBuilder: (context, index) => StockCard(
-            isButtonMode: isButtonMode,
-            stockname: stockmemos[index].name,
-            ticker: stockmemos[index].ticker,
-            market: stockmemos[index].market,
-            memo: stockmemos[index].memo,
-            onDeleteChanged: () async {
-              await showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return CustomAlertDialog(
-                    title: "${stockmemos[index].name}を削除しますか？",
-                    buttonText: "OK",
-                    onPressed: () async {
-                      Navigator.of(context).pop();
-                      await dbhelp.deleteMemo(stockmemos[index].id!);
-                      _refreshMemos();
-                    },
-                  );
-                },
+        backgroundColor: appBarColor,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () async {
+              // 画面遷移の動きを変更
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  pageBuilder: (
+                    context,
+                    animation,
+                    secondaryAnimation,
+                  ) {
+                    return const SettingsPage();
+                  },
+                  transitionsBuilder: (
+                    context,
+                    animation,
+                    secondaryAnimation,
+                    child,
+                  ) {
+                    const Offset begin = Offset(1.0, 0.0); // 右から左
+                    // final Offset begin = Offset(-1.0, 0.0); // 左から右
+                    const Offset end = Offset.zero;
+                    final Animatable<Offset> tween = Tween(
+                      begin: begin,
+                      end: end,
+                    ).chain(
+                      CurveTween(curve: Curves.easeInOut),
+                    );
+                    final Animation<Offset> offsetAnimation =
+                        animation.drive(tween);
+                    return SlideTransition(
+                      position: offsetAnimation,
+                      child: child,
+                    );
+                  },
+                ),
               );
             },
-            // todo3 編集機能を追加
-            onEditChanged: () async {},
-            createdAt: null,
-            updatedAt: null,
           ),
+        ],
+        title: Text(
+          "${AppLocalizations.of(context)!.appName} G",
+          style: titleTextStyle20,
         ),
+      ),
+      body: Column(
+        children: [
+          AdBanner(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: MasonryGridView.count(
+                crossAxisCount: 2,
+                itemCount: stockmemos.length,
+                itemBuilder: (context, index) => StockCard(
+                  isButtonMode: isButtonMode,
+                  stockname: stockmemos[index].name,
+                  ticker: stockmemos[index].ticker,
+                  market: stockmemos[index].market,
+                  memo: stockmemos[index].memo,
+                  onDeleteChanged: () async {
+                    await showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return CustomAlertDialog(
+                          title: "${stockmemos[index].name}を削除しますか？",
+                          buttonText: "OK",
+                          onPressed: () async {
+                            Navigator.of(context).pop();
+                            await dbhelp.deleteMemo(stockmemos[index].id!);
+                            _refreshMemos();
+                          },
+                        );
+                      },
+                    );
+                  },
+                  onEditChanged: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditPage(
+                          stockmemo: stockmemos[index],
+                        ),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
+                  createdAt: null,
+                  updatedAt: null,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: buttonColor,
